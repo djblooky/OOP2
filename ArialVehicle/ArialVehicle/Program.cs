9630434b970116314aa56b48c1e70eba38e091de
Haley Phillips <haleyphillips928@gmail.com>
2019-02-03 20:51:01 -0600

    initial commit

---
+++ ArialVehicle/ArialVehicle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArialVehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
