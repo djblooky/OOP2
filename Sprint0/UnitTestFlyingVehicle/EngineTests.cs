30f96f1df65e354d3f47407c1a4e9b337930b192
haley.phillips <haleyphillips928@gmail.com>
2019-02-04 17:14:03 -0600

    unit tests notes

--- Sprint0/UnitTestFlyingVehicle/EngineTests.cs
+++ Sprint0/UnitTestFlyingVehicle/EngineTests.cs
@@ -9,6 +9,17 @@ namespace UnitTestFlyingVehicle
     [TestClass]
     public class EngineTests
     {
+        [TestMethod]
+        public void EngineIsStarted() //open up test explorer window for green lights
+        {
+            //arrange
+            Engine e = new Engine(); //one engine per class
 
+            //act
+            bool defaultEngineStarted = e.IsStarted;
+
+            //assert
+            Assert.AreEqual(, true);
+        }
     }
 }
