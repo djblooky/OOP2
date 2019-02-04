b51c084d4edf7a89fcd2a6e94d30de94ebc98d1b
Haley Phillips <haleyphillips928@gmail.com>
2019-02-03 20:57:20 -0600

    renamed project

---
+++ ArialVehicle/ArialVehicle/ArialVehicle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArialVehicle
+{
+    class ArialVehicle
+    {
+    }
+}
--- ArialVehicle/ArialVehicle/Program.cs
+++ ArialVehicle/ArialVehicle/Program.cs
@@ -7,6 +7,7 @@ namespace ArialVehicle
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
