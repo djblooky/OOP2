db4d4a702a1c4807698f2e05927d3558679a94bd
haley.phillips <haley.phillips@IAM-WORK-13>
2019-02-11 17:07:04 -0600

    mammal class rough work

---
+++ DogInClassSprint1/DogInClassSprint1/Mammal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogInClassSprint1
+{
+    public class Mammal
+    {
+        public int Age { get; protected set; }
+        public int Weight { get; set; }
+        public string Name { get; protected set; }
+        public string Sound { get; protected set; }
+        public int soundCount { get; protected set; }
+
+        public char soundSplit = ',';
+
+        public Mammal()
+        {
+           
+        }
+
+        public void HappyBirthday()
+        {
+            this.Age++;
+        }
+
+        public string About()
+        {
+            return String.Empty;
+        }
+
+        public string Sound()
+        {
+            this.soundCount++;
+            return this.Sound;
+        }
+
+        public string MakeSound(int HowManyTimes)
+        {
+            string soundString = "";
+            for (int i = 0; i < HowManyTimes; i++)
+            {
+                soundString += this.Sound() + soundSplit;
+            }
+
+            return soundString;
+        }
+    }
+}
