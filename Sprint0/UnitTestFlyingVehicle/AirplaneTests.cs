913eed2c691992c1ee156c11b995ddbc8aca6c58
Haley Phillips <haleyphillips928@gmail.com>
2019-02-10 19:07:11 -0600

    added unit test classes

---
+++ Sprint0/UnitTestFlyingVehicle/AirplaneTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFlyingVehicle
+{
+    [TestClass]
+    public class AirplaneTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Sprint0/UnitTestFlyingVehicle/EngineTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFlyingVehicle
+{
+    [TestClass]
+    public class EngineTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
