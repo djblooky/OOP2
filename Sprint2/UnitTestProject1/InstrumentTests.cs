ab59485f1396ccc068ad411b5688bb078fc931fe
Haley Phillips <haleyphillips928@gmail.com>
2019-02-18 00:18:11 -0600

    blank instrument tests

---
+++ Sprint2/UnitTestProject1/InstrumentTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InstrumentTests
+{
+    [TestClass]
+    public class InstrumentTests
+    {
+        [TestMethod]
+        public void TestInstrumentAbout()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestInstrumentConstructor()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestInstrumentPlay()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestInstrumentStopPlaying()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestInstrumentGetSound()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestInstrumentMakeSound()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestInstrumentMakeSound(int HowManyTimes)
+        {
+
+        }
+
+        [TestMethod]
+        public void TestInstrumentGetIsPlayingString()
+        {
+
+        }
+    }
+}
