b33957fc91585501938f41488e9b82cd90da7ed6
Haley Phillips <haleyphillips928@gmail.com>
2019-02-18 01:08:07 -0600

    mostly finished instrument tests

--- Sprint2/UnitTestProject1/InstrumentTests.cs
+++ Sprint2/UnitTestProject1/InstrumentTests.cs
@@ -20,42 +20,64 @@ namespace InstrumentTests
         [TestMethod]
         public virtual void TestConstructor()
         {
+            i = new InstrumentTestClass();
 
+            Assert.AreEqual(false, i.IsPlaying);
         }
 
         [TestMethod]
-        public void TestPlay()
+        public void TestPlay() //finish?
         {
+            i = new InstrumentTestClass();
 
+            i.Play();
+
+            Assert.AreEqual(true, i.IsPlaying);
+            //test makesound ?
         }
 
         [TestMethod]
         public void TestStopPlaying()
         {
+            i = new InstrumentTestClass();
 
+            i.StopPlaying();
+
+            Assert.AreEqual(false, i.IsPlaying);
         }
 
         [TestMethod]
         public void TestGetSound()
         {
+            i = new InstrumentTestClass();
 
+            int OriginalSoundCount = i.SoundCount;
+            i.GetSound();
+            int NewSoundCount = i.SoundCount;
+
+            Assert.AreEqual(NewSoundCount, OriginalSoundCount + 1);
+            Assert.AreEqual(i.Sound, i.GetSound());
         }
 
         [TestMethod]
         public void TestMakeSound()
         {
+            i = new InstrumentTestClass();
+
+            Assert.AreEqual(i.MakeSound(3), i.MakeSound());
+        }
+
+        [TestMethod]
+        public void TestMakeSound(int HowManyTimes) //finish
+        {
+            i = new InstrumentTestClass();
 
         }
 
         [TestMethod]
-        public void TestMakeSound(int HowManyTimes)
+        public void TestGetIsPlayingString() //finish
         {
-
-        }
-
-        [TestMethod]
-        public void TestGetIsPlayingString()
-        {
+            i = new InstrumentTestClass();
 
         }
     }
