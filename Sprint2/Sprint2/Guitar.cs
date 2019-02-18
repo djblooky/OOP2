8c03331f63f70a73d3b61b09ea91c1656ea648fc
Haley Phillips <haleyphillips928@gmail.com>
2019-02-17 23:53:26 -0600

    basic classes and methods done

---
+++ Sprint2/Sprint2/Guitar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint2
+{
+    public class Guitar : StringInstrument
+    {
+        public Guitar()
+        {
+            Sound = "Strum";
+            NumberOfStrings = 6;
+        }
+    }
+}
---
+++ Sprint2/Sprint2/Ukulele.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint2
+{
+    public class Ukulele : StringInstrument
+    {
+        public Ukulele()
+        {
+            Sound = "Small Strum";
+            NumberOfStrings = 4;
+        }
+    }
+}
