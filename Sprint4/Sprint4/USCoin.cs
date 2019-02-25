71797eab84d81bd92413fd498c5f27c22eef3310
haley.phillips <haley.phillips@IAM-WORK-21>
2019-02-25 17:45:39 -0600

    added template classes

---
+++ Sprint4/Sprint4/USCoin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint4
+{
+    abstract class USCoin : Coin
+    {
+
+    }
+}
---
+++ Sprint4/Sprint4/USCoinMintMark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint4
+{
+    enum USCoinMintMark
+    {
+        P,
+        D,
+        S,
+        W
+    }
+}
