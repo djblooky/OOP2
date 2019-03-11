82cebb6f4480d12297284344a147d4d5853cd7a0
haley.phillips <haley.phillips@IAM-WORK-17>
2019-03-11 16:02:51 -0500

    added icurrencyrepo and implemented it

--- Sprint4/Sprint4/CurrencyRepo.cs
+++ Sprint4/Sprint4/CurrencyRepo.cs
@@ -6,5 +6,41 @@ namespace Sprint4
 {
     public class CurrencyRepo : ICurrencyRepo
     {
+        public List<ICoin> Coins { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string About()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCoin(ICoin c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCoinCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICurrencyRepo MakeChange(double Amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICurrencyRepo MakeChange(double AmountTendered, double TotalCost)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICoin RemoveCoin(ICoin c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double TotalValue()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Sprint4/Sprint4/ICurrencyRepo.cs
+++ Sprint4/Sprint4/ICurrencyRepo.cs
@@ -6,5 +6,13 @@ namespace Sprint4
 {
     public interface ICurrencyRepo
     {
+        List<ICoin> Coins{ get; set; }
+        string About();
+        void AddCoin(ICoin c);
+        int GetCoinCount();
+        ICurrencyRepo MakeChange(double Amount);
+        ICurrencyRepo MakeChange(double AmountTendered, double TotalCost);
+        ICoin RemoveCoin(ICoin c);
+        double TotalValue();
     }
 }
