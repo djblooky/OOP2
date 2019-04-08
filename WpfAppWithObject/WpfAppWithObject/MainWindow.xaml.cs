2082df1be26163a49578a163f49a9139537cc33a
haley.phillips <haley.phillips@IAM-WORK-10>
2019-04-08 16:08:41 -0500

    main window xaml buttons

--- WpfAppWithObject/WpfAppWithObject/MainWindow.xaml.cs
+++ WpfAppWithObject/WpfAppWithObject/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfAppWithObject
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
