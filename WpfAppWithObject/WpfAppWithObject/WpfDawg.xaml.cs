519c156ce456dc8fe625ae1be661ef7d235c3d2e
haley.phillips <haley.phillips@IAM-WORK-10>
2019-04-08 16:27:49 -0500

    added blank windows and classes

---
+++ WpfAppWithObject/WpfAppWithObject/WpfDawg.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System;
+using WpfAppWithObject.Models;
+using WpfAppWithObject.ViewModels;
+
+namespace WpfAppWithObject
+{
+    /// <summary>
+    /// Interaction logic for WpfDawg.xaml
+    /// </summary>
+    public partial class WpfDawg : Window
+    {
+        public WpfDawg()
+        {
+            InitializeComponent();
+        }
+    }
+
+    private void FoodViewControl_Loaded(object sender, RoutedEventArgs e)
+    {
+        ViewModelFood viewControl = new WpfAppWithObject.ViewModels.ViewModelFood();
+        viewControl.LoadFood();
+
+        this.DataContext = viewControl.Food[0];
+        FoodView1.DataContext = viewControl.Food[0];
+    }
+}
