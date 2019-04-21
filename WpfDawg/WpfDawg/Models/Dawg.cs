35533439e2ee0153f5dabd1be4263b2b98b7e42f
Haley Phillips <haleyphillips928@gmail.com>
2019-04-21 16:46:10 -0500

    created serializable dawg

---
+++ WpfDawg/WpfDawg/Models/Dawg.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDawg.Models
+{
+    [Serializable]
+    public class Dawg : ISerializable
+    {
+        public string Name { get; protected set; }
+        public double Cost { get; protected set; }
+        public bool IsVegan { get; protected set; }
+
+        public Dawg()
+        {
+            Name = "Devil Dawg";
+            Cost = 5.99;
+            IsVegan = true;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
