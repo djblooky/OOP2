a312ecaa95e6d68c8776450044572215d564ee63
Haley Phillips <haleyphillips928@gmail.com>
2019-04-28 21:17:29 -0500

    fix observable collection

--- WpfCurrencyMidterm/WpfCurrencyMidterm/Models/SaveableCurrencyRepo.cs
+++ WpfCurrencyMidterm/WpfCurrencyMidterm/Models/SaveableCurrencyRepo.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfCurrencyMidterm.Models
 {
-    public class SaveableCurrencyRepo
+    [Serializable]
+    public class SaveableCurrencyRepo : CurrencyRepo
     {
+        public string Path { get; set; }
+
         public SaveableCurrencyRepo(List<ICoin> coins)
         {
+            Path = "MyFile.bin";
+            Coins = coins;
+        }
 
+        public void Save()
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(Path, FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, Coins);
+            stream.Close();
+        }
+
+        public List<ICoin> Load()
+        {
+            Stream stream = new FileStream(Path, FileMode.Open, FileAccess.Read, FileShare.Read);
+            IFormatter formatter = new BinaryFormatter();
+            List<ICoin> coins = (List<ICoin>)formatter.Deserialize(stream);
+            stream.Close();
+            return coins;
         }
 
     }
--- WpfCurrencyMidterm/WpfCurrencyMidterm/ViewModels/MakeChangeViewModel.cs
+++ WpfCurrencyMidterm/WpfCurrencyMidterm/ViewModels/MakeChangeViewModel.cs
@@ -1,17 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WpfCurrencyMidterm.Models;
+﻿using WpfCurrencyMidterm.Models;
 
 namespace WpfCurrencyMidterm.ViewModels
 {
     public class MakeChangeViewModel : BaseViewModel
     {
+        private ICurrencyRepo repo;
+        private double amount = 0;
+        public BasicCommand basicCommand { get; private set; }
+        private SaveableCurrencyRepo saveRepo;
+        private ObservableCollection<ICoin> vmCoins = new ObservableCollection<ICoin>();
+
+
         public MakeChangeViewModel(ICurrencyRepo repo)
         {
+            this.repo = repo;
+        }
 
+        public ObservableCollection<ICoin> VMCoins
+        {
+            get { return vmCoins; }
+            set { vmCoins = value; RaisedPropertyChanged("VMCoins"); }
+        }
+
+        public BasicCommand SaveCoin
+        {
+            get
+            {
+                basicCommand = new BasicCommand(saveCoin);
+                return basicCommand;
+            }
+        }
+
+        public void saveCoin()
+        {
+            saveRepo = new SaveableCurrencyRepo(this.repo.Coins);
+            this.saveRepo.Save();
+        }
+
+        public BasicCommand Ocoin
+        {
+            get
+            {
+                if (basicCommand == null)
+                {
+                    basicCommand = new BasicCommand(OCoin);
+                }
+                return basicCommand;
+            }
+        }
+
+        private void OCoin()
+        {
+            this.repo = this.repo.MakeChange(Amount);
+            for (int i = 0; i < this.repo.GetCoinCount(); i++)
+            {
+                vmCoins.Add(this.repo.Coins[i]);
+            }
+        }
+
+        //This will represent the value the user input into the textbox
+        public double Amount
+        {
+            get
+            {
+                return this.amount;
+            }
+            set
+            {
+                RaisedPropertyChanged("Amount");
+                this.amount = value;
+            }
         }
     }
 }
