f5144af5bc4f9c1d64e8a62161d91f67f369d961
haley.phillips <haleyphillips928@gmail.com>
2019-05-01 20:28:06 -0500

    init commit

---
+++ BasicMVCSite/BasicMVCSite/Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicMVCSite.Controllers
+{
+    public class HelloController : Controller
+    {
+        public string Index()
+        {
+            return this.Hello("Haley");
+        }
+
+        public string Hello(string Name, int ?Age=0)
+        {
+            return $"Hello there! {Name} I am {Age} years old.";
+        }
+
+        public string Goodbye()
+        {
+            return "Bye!";
+        }
+    }
+}
