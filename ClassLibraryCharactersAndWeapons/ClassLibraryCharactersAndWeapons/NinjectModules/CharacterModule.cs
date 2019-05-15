a6fa51ac628bbdeca5ba6ee67fcb844750b9bbab
Haley Phillips <haleyphillips928@gmail.com>
2019-05-15 17:15:35 -0500

    added samurai inject

---
+++ ClassLibraryCharactersAndWeapons/ClassLibraryCharactersAndWeapons/NinjectModules/CharacterModule.cs
@@ -0,0 +1,14 @@
+﻿using Ninject.Modules;
+
+namespace ClassLibraryCharactersAndWeapons.NinjectModules
+{
+    public class CharacterModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IWeapon>().To<Sword>(); //any character that needs an Iweapon will get a sword
+            Bind<IWeapon>().To<Katana>().WhenInjectedExactlyInto<Samurai>();
+
+        }
+    }
+}
---
+++ ClassLibraryCharactersAndWeapons/UnitTestProjectDITest/CharacterTestNinject.cs
@@ -0,0 +1,29 @@
+﻿using ClassLibraryCharactersAndWeapons;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+
+namespace UnitTestProjectDITest
+{
+    [TestClass]
+    class CharacterTestNinject
+    {
+        IKernel k;
+
+        [TestMethod]
+        public void SamuraiInject()
+        {
+            //arrange
+            Character c;
+            string attackTarget = "bad guy";
+
+            //act
+            c = k.Get<Samurai>();
+
+            //assert
+            Assert.IsInstanceOfType(c, typeof(Character));
+            Assert.IsInstanceOfType(c, typeof(Samurai));
+            Assert.IsInstanceOfType(c.Weapon, typeof(Katana));
+            Assert.AreEqual(c.Attack(attackTarget), $"{c.Weapon.Name} hits {attackTarget}");
+        }
+    }
+}
