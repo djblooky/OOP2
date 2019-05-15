4d8b6c929d1953a367fbd4fcd108d5f8cb5b13b4
Haley Phillips <haleyphillips928@gmail.com>
2019-05-14 23:26:56 -0500

    weapon class and interface

---
+++ ClassLibraryCharactersAndWeapons/ClassLibraryCharactersAndWeapons/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibraryCharactersAndWeapons
+{
+    public abstract class Weapon : IWeapon
+    {
+        protected string name;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public string Hit(string target)
+        {
+            return $"{name} hits {target}";
+        }
+
+        public Weapon()
+        {
+            name = "not a weapon!";
+        }
+    }
+}
