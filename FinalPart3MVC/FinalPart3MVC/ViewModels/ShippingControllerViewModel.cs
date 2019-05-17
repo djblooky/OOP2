ba32ac8eff57c42332bc2461e482d121e5b11037
Haley Phillips <haleyphillips928@gmail.com>
2019-05-16 23:16:13 -0500

    added form html and new viewmodel

---
+++ FinalPart3MVC/FinalPart3MVC/ViewModels/ShippingControllerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalPart3MVC.ViewModels
+{
+    public class ShippingControllerViewModel
+    {
+        public string Test { get; set; }
+        public List<string> ShippingDDLNames { get; set; }
+
+        public ShippingControllerViewModel()
+        {
+            Test = "Yee haw from Shipping View Model!";
+            ShippingDDLNames = new List<string> { "yee", "haw", "howdy"};
+        }
+    }
+}
